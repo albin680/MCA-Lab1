acd8e8e29e47080ec651914838241fa5d24afb17
albin680 <150990918+albin680@users.noreply.github.com>
2024-02-28 13:29:17 +0530

    Add files via upload

---
+++ s2/JAVA/Example3.java
@@ -0,0 +1,15 @@
+class Example3{
+	public static void main(String args[])
+		{
+			int x,y;
+			x=10;
+			y=20;
+			if(x<y)System.out.println("x is less than y");
+			x=x*2;
+			if(x==y)System.out.println("x is now equal to y");
+			x=x*2;
+			if(x>y)System.out.println("x is greater than y");
+			if(x==y)System.out.println("you wont see this");
+		}
+	}
+			
